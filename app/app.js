a94abc1ed85939df8040e66b839e3fc57190d7e3
euijo <gogogosc1234@hanmail.net>
2022-01-24 23:10:08 +0900

    프런트단 자바스크립트 파일 만들기

--- app/app.js
+++ app/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 
-
-
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 셋팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
 app.use("/", home);
 
 module.exports = app;
