ac38fc5c5f5694ae7b15854baacc06e5e9f8e9e4
euijo <gogogosc1234@hanmail.net>
2022-01-27 05:21:13 +0900

    file sys 구현

--- app/src/modles/UserStorage.js
+++ app/src/modles/UserStorage.js
@@ -1,12 +1,8 @@
-class UserStorage{
-    static #user = {
-        id : ["gigisdf", "go2342", "김사랑"],
-        password : ["1111", "2222", "333333"],
-        name : ["김장선", "와나나", "박지"]
-    };
+const fs = require("fs");
 
+class UserStorage{
     static getUers(...fileds){
-        const users = this.#user;
+        // const users = this.#user;
         const newUsers = fileds.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -17,17 +13,20 @@ class UserStorage{
     }
 
     static getUserInfo(id){
-        const users = this.#user;
-        const idx = users.id.indexOf(id);
-        const userInfo = Object.keys(users).reduce((newUser, info) => {
+        // const users = this.#user;
+        fs.readFile("./src/databases/users.json", (err, data) => {
+            if(err) throw err;
+            const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const userInfo = Object.keys(users).reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         },{});
-
-        return userInfo;
+            return userInfo;
+        });
     }
     static save(userInfo){
-        const users = this.#user;
+        // const users = this.#user;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
